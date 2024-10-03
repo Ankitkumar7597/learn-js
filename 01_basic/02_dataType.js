ca1e95dfc0a6b6567ff313f9bf8b00c90a917a16
Ankit kumar <119477154+Ankitkumar7597@users.noreply.github.com>
2024-10-03 07:26:27 +0000

    dataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js  code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+let name = "ankit"; //string
+let age = 18; // constaint
+let isLoggedIn = true //boolean
+
+// dataType
+
+// number => 2 to power 53
+// bigint => bigger value store from number
+// string => ""
+// boolean => true/false
+// null = standalone value
+// undefined =>
+// symbol => 
+
+
+
+// object =>
+
+console.log(typeof (undefined))  // undefined
+console.log(typeof (null))       // null
