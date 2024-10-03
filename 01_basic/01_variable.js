9a1f1f000eb412d0c8803dedebf28038985eeefc
Ankit kumar <119477154+Ankitkumar7597@users.noreply.github.com>
2024-10-03 07:15:12 +0000

    variable file

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,18 @@
+var city = 'jaipur'
+var number = '1'
+let email = 'ankit@gmail.com'
+const accountId = 123
+state = 'Rajasthan'
+
+let checkvalue;
+
+checkvalue='start'
+/* 
+Prefer not to use var
+because of issue in block scope or funtional scope
+*/
+
+// console.log(number + accountId)
+// console.table([city,number,email,accountId,state])
+// console.log(typeof (accountId));
+console.log('checkvalue', checkvalue)
